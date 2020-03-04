db7f1a054f938930bb51e68e6ebd5b2796eb6a84
estm3 <60245369+estm3@users.noreply.github.com>
2020-03-03 19:58:55 -0600

    Add files via upload

---
+++ Practica10/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body> 
+
+<?php
+
+//comentario 1 linea
+/*comentario
+multilinea*/ 
+
+echo "<h1>Hola mundo PHP</h1>"
+?>
+
+</body>
+</html>
+
+
